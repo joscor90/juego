4b135de9a5082d99ca1633495fa3e55757771cc9
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-08-19 15:21:28 -0400

    Añadida la variable de la eleccion al azar de la computadora

--- juego.rb
+++ juego.rb
@@ -1,5 +1,8 @@
-#Input del usuario
+#Input del usuario.
 input_usuario = ARGV[0].to_s
 
-#Arreglo de opciones para la computadora
+#Arreglo de opciones para la computadora.
 opciones = ["piedra", "papel", "tijera"]
+
+#Esta es la elección que hace la computadora a partir del arreglo de opciones disponible.
+eleccion = arreglo.[rand(3)]
