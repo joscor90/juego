30634572bb4150b525e963b3b64bf674b1859068
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-08-19 15:27:43 -0400

    Añadido el flujo de control y downcase al input del usuario

--- juego.rb
+++ juego.rb
@@ -1,8 +1,34 @@
 #Input del usuario.
 input_usuario = ARGV[0].to_s
+input_usuario.downcase!
 
 #Arreglo de opciones para la computadora.
 opciones = ["piedra", "papel", "tijera"]
 
 #Esta es la elección que hace la computadora a partir del arreglo de opciones disponible.
 eleccion = arreglo.[rand(3)]
+
+#Flujo de control del juego.
+if input_usuario == eleccion
+    puts "Computador juega #{eleccion}"
+    puts "Empataste"
+elsif input_usuario == "piedra" && eleccion == "tijera"
+    puts "Computador juega #{eleccion}"
+    puts "Ganaste"
+elsif input_usuario == "piedra" && eleccion == "papel"
+    puts "Computador juega #{eleccion}"
+    puts "Perdiste"
+elsif input_usuario == "papel" && eleccion == "piedra"
+    puts "Computador juega #{eleccion}"
+    puts "Ganaste"
+elsif input_usuario == "papel" && eleccion == "tijera"
+    puts "Computador juega #{eleccion}"
+    puts "Perdiste"
+elsif input_usuario == "tijera" && eleccion == "papel"
+    puts "Computador juega #{eleccion}"
+    puts "Ganaste"
+elsif input_usuario == "tijera" && eleccion == "piedra"
+    puts "Computador juega #{eleccion}"
+    puts "Perdiste"
+else
+    puts "Argumento inválido: debe ser piedra, papel o tijera"
