c7204ab35d85a48bf3b8efebd0e7aa8c07209597
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-08-24 21:53:58 -0400

    last commit

--- juego.rb
+++ juego.rb
@@ -7,19 +7,25 @@ opciones = ["piedra", "papel", "tijera"]
 #Esta es la elección que hace la computadora a partir del arreglo de opciones disponible.
 eleccion = opciones[rand(3)]
 
+#Método de "Computador Juega"
+def comp_juega(e)
+    return "Computador juega #{e}"
+end
+
 #Flujo de control del juego.
 case(input_usuario)
 when eleccion
-    puts "Computador juega #{eleccion}.\nEmpataste"
+    puts comp_juega(eleccion)+".\nEmpataste"
 when "tijera"
-    puts "Computador juega #{eleccion}.\nGanaste" if eleccion == "papel"
-    puts "Computador juega #{eleccion}.\nPerdiste" if eleccion == "piedra"
+    puts comp_juega(eleccion)+".\nGanaste" if eleccion == "papel"
+    puts comp_juega(eleccion)+".\nPerdiste" if eleccion == "piedra"
 when "piedra"
-    puts "Computador juega #{eleccion}.\nGanaste" if eleccion == "tijera"
-    puts "Computador juega #{eleccion}.\nPerdiste" if eleccion == "papel"
+    puts comp_juega(eleccion)+".\nGanaste" if eleccion == "tijera"
+    puts comp_juega(eleccion)+".\nPerdiste" if eleccion == "papel"
 when "papel"
-    puts "Computador juega #{eleccion}.\nGanaste" if eleccion == "piedra"
-    puts "Computador juega #{eleccion}.\nPerdiste" if eleccion == "tijera"
+    puts comp_juega(eleccion)+".\nGanaste" if eleccion == "piedra"
+    puts comp_juega(eleccion)+".\nPerdiste" if eleccion == "tijera"
 else
     puts "Argumento inválido: debe ser piedra, papel o tijera"
 end
+
